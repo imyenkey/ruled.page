3bb11574b7b4ed8217b53675837a1aec0df81d28
imyenkey <ping@yenkey.in>
2023-12-18 18:40:10 +0530

    git commit -m

--- app.js
+++ app.js
@@ -1,70 +1,15 @@
-const menuToggle = document.getElementById('menu-toggle');
-const menu = document.getElementById('menu');
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-
-// Menu toggle functionality
-menuToggle.addEventListener('click', () => {
-  menu.classList.toggle('open'); // Add/remove 'open' class to toggle menu visibility
-});
-
-// Note title and content input fields
-const noteTitleInput = document.getElementById('note-title');
-const noteContentInput = document.getElementById('note-content');
-
-// Function to save note data in browser cache
-function saveNoteToCache() {
-  const title = noteTitleInput.value;
-  const content = noteContentInput.value;
-
-  // Convert note data to JSON string
-  const noteData = JSON.stringify({ title, content });
-
-  // Store note data in browser cache under a specific key
-  localStorage.setItem('ruledPageNote', noteData);
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 }
 
-// Function to load note data from browser cache
-function loadNoteFromCache() {
-  const noteData = localStorage.getItem('ruledPageNote');
+function toggleTheme() {
+  const container = document.querySelector('.container');
+  container.classList.toggle('dark-theme');
 
-  if (noteData) {
-    // Parse JSON string back to object
-    const parsedNote = JSON.parse(noteData);
+  const lightMode = document.getElementById('lightMode');
+  const darkMode = document.getElementById('darkMode');
 
-    // Update input fields with cached note data
-    noteTitleInput.value = parsedNote.title;
-    noteContentInput.value = parsedNote.content;
-  }
+  lightMode.style.display = lightMode.style.display === 'block' ? 'none' : 'block';
+  darkMode.style.display = darkMode.style.display === 'block' ? 'none' : 'block';
 }
-
-// Save note data to cache when form is submitted
-const form = document.querySelector('.new-note form');
-form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent form submission
-
-  saveNoteToCache();
-
-  // Clear input fields after saving
-  noteTitleInput.value = '';
-  noteContentInput.value = '';
-
-  // You can also display a success message or redirect to another page here
-});
-
-// Load note data from cache when the page loads
-window.onload = loadNoteFromCache;
-
-// Clear note data from cache if desired (uncomment to enable)
-// localStorage.removeItem('ruledPageNote');
-
-// Dark mode toggle functionality (add logic or CSS classes to implement dark mode)
-darkModeToggle.addEventListener('click', () => {
-  // Your dark mode logic goes here
-});
-
-// Note that this code example doesn't integrate with the HTML structure or implement dynamic note list functionalities. 
-// These aspects require further development based on your specific design and desired features.
-
-// Remember, you can extend this code with additional functionalities and connect it to your HTML and CSS to create a fully functional note-taking app.
-
-//I hope this clarifies the code and provides a good starting point for your development process. Feel free to ask any further questions as you progress!
